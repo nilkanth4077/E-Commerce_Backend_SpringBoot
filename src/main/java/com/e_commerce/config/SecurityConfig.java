97ade29af8e3084bcf011367973fcb4caf9d21cc
nilkanth4077 <nilkanth4077@gmail.com>
2025-02-16 17:38:40 +0530

    Cors site update

--- src/main/java/com/e_commerce/config/SecurityConfig.java
+++ src/main/java/com/e_commerce/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
         cfg.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000", "http://127.0.0.1:5500"
+                "http://localhost:3000", "http://127.0.0.1:5500", "https://e-commerce-backend-springboot.onrender.com"
         ));
         cfg.setAllowedMethods(Collections.singletonList("*"));
         cfg.setAllowCredentials(true);
